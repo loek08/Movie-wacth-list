b075a2e4d40b63694c65a7aa32e7aa76e51ecbba
Loek Ruhe <558777@student.fontys.nl>
2025-11-11 09:02:16 +0100

    ui
    Integrate dynamic movie data into home view
    
    Refactored `MovieHomeController` to use dependency injection for `MovieService` and fetch movie data dynamically. Updated the `Index` action to pass a list of movies to the view. Modified `Index.cshtml` to use a strongly-typed model and render movie names dynamically. Simplified the layout by removing unused sections and updated the header and footer text for consistency.

--- Movie wacth list/Controllers/MovieHomeController.cs
+++ Movie wacth list/Controllers/MovieHomeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LogicLayer.Models;
+using LogicLayer.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Movie_wacth_list.Controllers
 {
     public class MovieHomeController : Controller
     {
+        private readonly MovieService _movieService;
+
+        public MovieHomeController(MovieService movieService)
+        {
+            _movieService = movieService ?? throw new ArgumentNullException(nameof(movieService));
+        }
         public IActionResult Index()
         {
-            return View();
+            var movies = _movieService.GetAllMovies() ?? Enumerable.Empty<Movie>();
+            return View(movies.ToList());
         }
     }
 }
